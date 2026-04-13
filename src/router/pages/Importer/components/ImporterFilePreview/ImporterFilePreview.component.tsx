08d8efd07a9fb40bd3c9251921caf9d789c87e2a
blksnk <art.jnveigel@gmail.com>
2026-04-13 14:45:12 +0200

    fix: tooltip wording

--- src/router/pages/Importer/components/ImporterFilePreview/ImporterFilePreview.component.tsx
+++ src/router/pages/Importer/components/ImporterFilePreview/ImporterFilePreview.component.tsx
@@ -159,7 +159,7 @@ function FilePreviewContents({
                 <Show
                   if={STORES.schema.columns[name]}
                   else={() => (
-                    <Tooltip content="This column's data will be save in your browser.">
+                    <Tooltip content="This column's data will be saved upon upload.">
                       <Badge theme="blue">New column</Badge>
                     </Tooltip>
                   )}
